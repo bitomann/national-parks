45d94845ec479e32ac88349b2a4eea64a3c0a021
Bito Mann <bito@bitomann.com>
2019-10-25 16:09:00 -0500

    attempting to run for each loop and print to the DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,50 @@
-console.log("What the f***")
+console.log("What the f***:)")
+
+// // document.querySelector("#saveEntry").addEventListener("click", event => {
+// //     // console.log("button was clicked!!!!");
+//     const personName = document.querySelector("#fullName").value;
+//     // console.log(personName)
+//     const address = document.querySelector("#address").value;
+//     // console.log(address)
+
+//     container.innerHTML += `
+//         <section>
+//             <h1>${personName}</h1>
+//             <div>${address}</div>
+//         </section>
+//     `
+// })
 
 
-const nationalParksComponent = () => {
+// Built html elements to append to the DOM //
+const nationalParksComponent = (parseParks) => {
+
+    console.log(parseParks)
     return `
     <article>
         <fieldset>
-            <h3>Park Name</h3>
-            <p>State the park in located in</p>
+            <h3 for= parkName>Park Name ${parseParksname}</h3>
+            <p for= parkState>State the park in located in ${parseParks.name}</p>
         </fieldset>   
     </article>
     `
 };
+console.log(nationalParksComponent)
 
+// pointed to the <body> element //
 const nationalParksContainer = document.querySelector("body")
 
-nationalParksContainer.innerHTML = nationalParksComponent()
+// injected variable to the DOM //
 
+// fetched and parsed data form the parks.json server and displayed it in console //
 fetch("http://localhost:8088/parks")
-    .then(parks => parks.json())
-    .then(parseParks => {
-        console.table(parseParks)
-    });
-
-    
-
+.then(parks => parks.json())
+.then(parseParks => {
+    console.log(parseParks)
+    parseParks.forEach(element => {
+        console.log(element)
+        nationalParksComponent(element)
+        nationalParksContainer.innerHTML = nationalParksComponent()
+        
+    })
+})
